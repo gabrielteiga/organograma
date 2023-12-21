c676f836dc17b6fd14f843608eec101a6107d484
Gabriel Teiga <gabrielteiga99@gmail.com>
2023-12-21 04:07:59 -0300

    Base de funcionalidades OK

---
+++ src/components/Employee/Employee.js
@@ -0,0 +1,15 @@
+import './Employee.css'
+
+export const Employee = ({name, role, image}) => {
+    return(
+        <div className='employee'>
+            <div className='header'>
+                <img src={image} alt={name} />
+            </div>
+            <div className='footer'>
+                <h4>{name}</h4>
+                <h5>{role}</h5>
+            </div>
+        </div>
+    )
+}
